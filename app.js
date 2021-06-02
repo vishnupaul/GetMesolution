e78ec32df799e036751d030b1abf34ce87a6d782
Vishnu Paul <paulvishnu567@gmail.com>
2021-06-02 12:37:25 +0530

    setup staic files access for layouts and pages

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
-
+const expressLayouts = require('express-ejs-layouts');
 const PORT = process.env.PORT || 8000;
+
+app.use(express.static('./assets'));
+//
+app.use(expressLayouts);
+// extract style and scripts from sub page into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+// use express router
+app.use('/', require('./routes'));
+
+//setup view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(PORT, console.log(`Server started On port ${PORT}`));
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+const homeController = require('../controllers/home_controller');
+
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
+console.log('router loaded');
+
+module.exports = router;
