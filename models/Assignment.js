1ee173079ed5e0360222ddbbc955b8f35652a7a2
Vishnu Paul <paulvishnu567@gmail.com>
2021-06-02 21:55:14 +0530

    create feedback model and show the feedback in homepage

---
+++ models/Assignment.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const assignmentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Assignment = mongoose.model('Assignment', assignmentSchema);
+module.exports = Assignment;
